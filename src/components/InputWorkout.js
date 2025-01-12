af633be787be38a2a4063df13e6aaf2411a1c4d2
theo <thjmaurino@gmail.com>
2025-01-12 15:53:52 -0600

    :)

---
+++ src/components/InputWorkout.js
@@ -0,0 +1,113 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react'
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+const InputWorkout = () => {
+
+
+    const [startTime, setStartTime] = useState(null);
+    const [endTime, setEndTime] = useState(null);
+
+
+    const [exerciseInput, setExerciseInput] = useState([]);
+    const [exercises, setExercises] = useState([]);
+
+    // exercises === [{"exercise" : 4}, {"exercise" : 10}, ... ]
+    // exercises === [{name: "pull up", reps : 4}, ]
+
+    const handleCommitExercise = () => {
+        // push excercise stored in ererciseInput into exercises (rendering it)
+        // clear exercise inpuyt
+    }
+
+    const handleExerciseChange = (event) => {
+        setExerciseInput({
+            ...exercises,
+            name : event.target.value
+        })
+
+        // handle any validation or whatever else here
+        console.log(exerciseInput);
+        
+    }
+
+    const handleRepsChange = (event) => {
+        setExerciseInput({
+            ...exercises,
+            reps : event.target.value
+        })
+
+        // handle any validation or whatever else here
+        console.log(exerciseInput);
+    }
+
+
+
+  return (
+    <div className='input-component w-full flex flex-col justify-center items-center'>
+        <form>
+            <div className='flex flex-col justify-center items-center w-full'>
+
+                <label htmlFor='start-time' className='text-app_white'>Start Time</label>
+                <input id='start-time' type='datetime-local' />
+
+                <label htmlFor='end-time' className='text-app_white'>End Time</label>
+                <input id='end-time' type='datetime-local' />
+
+            </div>
+
+
+            <div className='exercise-input-field flex flex-col w-[90%]  max-h-[60%] mt-10'>
+
+                <div className='already-input-exercises flex flex-col justify-center items-center'>
+                    {
+                        exercises.map((exercise, idx) => (
+                            <div key={idx} className='exercise-in-input-list'>
+                                {Object.entries(exercise)}
+                            </div>
+                        ))
+                    }
+                </div>
+
+
+                <div className='flex flex-row gap-2'>
+
+                    <div className='flex flex-col'>
+                        <label htmlFor='input-exercise-name' className='text-app_white'>Exercise Name</label>
+                        <input 
+                            id='input-exercise-name' 
+                            type='text' 
+                            placeholder='...' 
+                            onChange={handleExerciseChange}
+                        />
+                    </div>
+
+                    <div className='flex flex-col'>
+                        <label htmlFor='input-reps' className='text-app_white'>Reps/Time</label>
+                        <input 
+                            id='input-reps' 
+                            type='number' 
+                            className='w-[20%]' 
+                            placeholder='0' 
+                            onChange={handleRepsChange}
+                        />
+                    </div>
+
+
+                    <div className='commit-workout rounded-full bg-app_teal flex flex-col justify-center items-center w-[40px] h-[40px]'>
+                        <FontAwesomeIcon className=' text-app_white ' icon={faPlus} />
+                    </div>
+                </div>
+
+
+
+                
+            </div>
+        
+        </form>
+      
+    </div>
+  )
+}
+
+export default InputWorkout
--- src/pages/add-workout.js
+++ src/pages/add-workout.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import InputWorkout from '@/components/InputWorkout';
+import React, { useState } from 'react'
 
 const AddWorkout = () => {
+
+  const [currentWorkout, setWorkout] = useState({});
+
   return (
     <div className='add-workout-page w-full h-full bg-app_background p-4 flex flex-col items-center justify-center'>
-        <h1 className='text-app_white text-center'>Add Workout Page Soon</h1>
+      
+      <InputWorkout />
+
+
       
     </div>
   )
